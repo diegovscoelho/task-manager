733da667c7bfdcbf8e499774af81f53a009edd41
diegovscoelho <dcoelhocarneiro@gmail.com>
2025-10-29 22:54:23 -0300

    Implementing Task entity and TaskStatus enum

---
+++ src/main/java/com/diego/task_manager/entities/Task.java
@@ -0,0 +1,37 @@
+package com.diego.task_manager.entities;
+
+import com.diego.task_manager.enums.TaskStatus;
+
+public class Task {
+
+    private Long id;
+    private String description;
+    private TaskStatus status;
+
+    public Task() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/diego/task_manager/enums/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.diego.task_manager.enums;
+
+public enum TaskStatus {
+    DONE,
+    NOT_DONE,
+    DOING,
+}
